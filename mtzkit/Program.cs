82b7d9a1f2014d405bafc2f6cb2d2f45f3af4c53
no3ku <vakkon23@gmail.com>
2025-11-12 14:52:35 +0200

    Оновлення програмного коду

--- mtzkit/Program.cs
+++ mtzkit/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Лабораторна работа з Git!");
+            Console.WriteLine("Автора: Резніченко Максим");		
         }
     }
 }
